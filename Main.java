4037ae67337189209a4e43eeef0460073ef394e1
fik4er <103965258+fik4er@users.noreply.github.com>
2025-03-31 16:50:00 +0300

    Update Main.java 1.2

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.println("Значемы изменения 1");
+        System.out.println("Значемы изменения 2");
     }
 }
