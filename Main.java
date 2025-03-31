e32ec3b6d004c330cfe81a0064fc89c063979b4d
fik4er <103965258+fik4er@users.noreply.github.com>
2025-03-31 18:05:31 +0300

    Изменение Main.java в ветке dev 1.2

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
         System.out.println("Значительное изменение 1");
+        System.out.println("Значительное изменение 2");
     }
 }
