a74f0f36e226243f94d685ba5db6b9fcfe41eea7
fik4er <103965258+fik4er@users.noreply.github.com>
2025-05-05 21:20:19 +0300

    Update Main.java in dev v4

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
         System.out.print("Изменение в ветке dev 1");
+        System.out.println(messageGen() + "использовано в 4 обновлении dev");
     }
     public static void newMethod() {
         System.out.println("Новый метод в dev");
