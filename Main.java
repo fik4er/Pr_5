17ab03d8ba8545c7f65fc5df7e2633e3976f0c0e
fik4er <103965258+fik4er@users.noreply.github.com>
2025-05-05 21:09:28 +0300

    Update Main.java in main v3

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.print("Изменение в ветке main 1");
+        System.out.println(messageGen() + "использовано в 4 обновлении");
     }
     public static void newMethod() {
         System.out.println("Новый метод");
